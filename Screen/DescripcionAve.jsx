df1eaf6eebabb2a4d41170774581abc39f6ceb6a
MarcoTzintzun <guitarra.tz5@gmail.com>
2024-02-20 23:34:34 -0600

    Navegacion para cambiar entre SeccionAves y DescripcionAve (Aun en proceso)

--- Screen/DescripcionAve.jsx
+++ Screen/DescripcionAve.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import fotoLoro from '../assets/paracho.png'; // Importa la imagen de manera adecuada
 
-const DescripcionAve = ({ foto, nombreCientifico, nombreComercial, peso, edadMaxima, lugaresVistos, descripcion }) => {
+const DescripcionAve = ({ navigation, id, foto, nombreCientifico, nombreComercial, peso, edadMaxima, lugaresVistos, descripcion }) => {
+  console.log("ESTO RECIBE DESCPCIONAVE",id, foto, nombreCientifico, nombreComercial, peso, edadMaxima, lugaresVistos, descripcion);
+
+  const volverAtras = () => {
+    navigation.goBack(); // Función para volver a la pantalla anterior
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={foto} style={styles.image} />
+      <Image source={fotoLoro} style={styles.image} />
       <Text style={styles.label}>Nombre Científico:</Text>
       <Text style={styles.text}>{nombreCientifico}</Text>
       <Text style={styles.label}>Nombre Comercial:</Text>
@@ -15,6 +22,9 @@ const DescripcionAve = ({ foto, nombreCientifico, nombreComercial, peso, edadMax
       <Text style={styles.text}>Lugares Vistos: {lugaresVistos}</Text>
       <Text style={styles.label}>Descripción:</Text>
       <Text style={styles.text}>{descripcion}</Text>
+      <TouchableOpacity onPress={volverAtras}>
+        <Text>Volver</Text> 
+      </TouchableOpacity>
     </View>
   );
 };
@@ -22,11 +32,13 @@ const DescripcionAve = ({ foto, nombreCientifico, nombreComercial, peso, edadMax
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0e0000',
     padding: 20,
+    height: "auto",
+    width: "100%",
   },
   image: {
-    width: '100%',
+    width: '50%',
     height: 200,
     marginBottom: 10,
   },
